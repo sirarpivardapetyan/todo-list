8f3f77606f70bc773b430e4cb71bf41ce507d461
sirarpivardapetyan <sirarpivardapetyan@gmail.com>
2023-03-20 17:40:44 +0400

    lesson64 by change

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Product from "./components/Product";
-import { Component } from "react";
 
-class App extends Component {
-  state = {
-    products: [
-      {
-        name: "bananas",
-        price: "1",
-        description: "Fresh bananas from Ecuador",
-      },
-      {
-        name: "pie",
-        price: "2",
-        description: "Fresh pies from Armenia",
-      },
-      {
-        name: "orange",
-        price: "5",
-        description: "Fresh oranges from Kanada",
-      },
-    ],
-  };
+import Todo from "./components/Todo";
 
-  render() {
-    const productComponents = this.state.products.map((product) => {
-      return (
-        <Product
-          key={product.name}
-          name={product.name}
-          price={product.price}
-          description={product.description}
-        />
-      );
-    });
 
-    return <div className="App">{productComponents}</div>;
-  }
-}
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="p-3 App-header" bg="success" text="white">
+        <div class="title d-flex justify-content-center align-items-center">
+          <h3 className="p-3">ToDoList</h3>
+        </div>
+        </header>
+        <main >
+        <Todo/>
+
+           
+        </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header width = "50">
+          <div>
+            <h3 className="p-3">ToDo List</h3>
+          </div>
+     
+      </header>
+    );
+  }
+}
+
+export default Header;
